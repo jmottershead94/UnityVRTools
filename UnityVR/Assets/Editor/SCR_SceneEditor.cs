4f9bf67111ff6d034fe86fcff5e5c9799266b54b
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-10-22 17:55:01 +0100

    Update - Moved project folder outside of nested UnityVR folder structure.

---
+++ UnityVR/Assets/Editor/SCR_SceneEditor.cs
@@ -0,0 +1,48 @@
+﻿// Unity includes here.
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SCR_SceneEditor : EditorWindow 
+{
+
+	[MenuItem("Window/Scene Editor/Enable")]
+	public static void Enable()
+	{
+		SceneView.onSceneGUIDelegate += OnScene;
+		Debug.Log("Scene Enabled");
+	}
+
+	[MenuItem("Window/Scene Editor/Disable")]
+	public static void Disable()
+	{
+		SceneView.onSceneGUIDelegate -= OnScene;
+		Debug.Log("Scene Disabled");
+	}
+
+	[MenuItem("Window/Scene Editor/Play Scene")]
+	public static void BuildAssetBundles()
+	{
+
+		EditorApplication.isPlaying = true;
+		EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Additive);
+
+	}
+
+	private static void OnScene(SceneView sceneView)
+	{
+
+		Handles.BeginGUI();
+
+		if(GUILayout.Button("PRESS ME!"))
+		{
+
+			Debug.Log("Got it to work.");
+
+		}
+
+		Handles.EndGUI();
+
+	}
+
+}
