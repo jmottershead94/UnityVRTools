43adebd7b1a30456288c868261e9bd1d7bf28544
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-11-21 21:26:27 +0000

    Update - The current lerp rotation works, apart from one overlapping angle, will work on it after object transform manipulation has been implemented.

--- UnityVR/Assets/Scripts/UI/Buttons/SCR_RotatePanelButton.cs
+++ UnityVR/Assets/Scripts/UI/Buttons/SCR_RotatePanelButton.cs
@@ -7,7 +7,11 @@ public class SCR_RotatePanelButton : SCR_3DButton
 	/* Attributes. */
 	[Header ("Panel Control")]
 	[SerializeField]	private Vector3 rotationValue = Vector3.zero;
+	[SerializeField]	private Vector3 rotationSpeed = Vector3.zero;
 	private SCR_3DMenu menu = null;
+	private int currentRotation = 0;
+	private const int targetAngle = 90;
+	private bool shouldRotate = false;
 
 	/* Methods. */
 	new void Awake()
@@ -19,15 +23,12 @@ public class SCR_RotatePanelButton : SCR_3DButton
 
 	}
 
-	/* This will allow us to define a specific button response. */
-	override protected void ButtonPressResponse()
+	void ArrowResponse()
 	{
-
-		/* Rotate all of the panels. */
-		menu.transform.FindChild("Panels").Rotate(rotationValue);
 
 		if(name == "Right Arrow")
 		{
+			//destinationRotation = new Vector3(0.0f, 90.0f, 0.0f);
 			menu.Panels[menu.CurrentPanelIndex].InFocus = false;
 
 			if(menu.CurrentPanelIndex < (menu.Panels.Count - 1))
@@ -55,4 +56,58 @@ public class SCR_RotatePanelButton : SCR_3DButton
 		}
 
 	}
+
+	/* This will allow us to define a specific button response. */
+	override protected void ButtonPressResponse()
+	{
+
+		/* Rotate all of the panels. */
+		menu.transform.FindChild("Panels").Rotate(rotationValue);
+
+		ArrowResponse();
+
+		/*shouldRotate = true;*/
+
+	}
+
+	void LerpRotation()
+	{
+
+		if(currentRotation < targetAngle)
+		{
+
+			currentRotation++;
+
+			/* menu.transform.FindChild("Panels").Rotate(rotationSpeed); */
+		}
+		else
+		{
+
+			currentRotation = 0;
+
+
+			shouldRotate = false;
+		}
+
+	}
+
+//	new void Update()
+//	{
+//
+//		base.Update();
+//
+//		if(shouldRotate)
+//		{
+//
+//			LerpRotation();
+//
+//		}
+//
+//	}
+
+	private bool LastPanel
+	{
+		get { return ((menu.CurrentPanelIndex == (menu.Panels.Count - 1)) || (menu.CurrentPanelIndex == 0)); }
+	}
+
 }
