30447384dc4ec4a0554a159f4a93ab6beb3b7524
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-11-28 21:56:07 +0000

    Update - Added comments to the detailed window class.

--- UnityVR/Assets/Scripts/UI/SCR_DetailedWindow.cs
+++ UnityVR/Assets/Scripts/UI/SCR_DetailedWindow.cs
@@ -1,24 +1,32 @@
-﻿using UnityEngine;
+﻿/*
+*
+*	Detailed Window Class
+*	=====================
+*
+*	Created: 	2016/11/21
+*	Filter:		Scripts/UI
+*	Class Name: SCR_DetailedWindow
+*	Base Class: SCR_BaseUIElement
+*	Author: 	1300455 Jason Mottershead
+*
+*	Purpose:	This class will be responsible for displaying the detailed information about 
+*				the current highlighted object, and will - later in development - provide more
+*				functionality.
+*
+*/
+
+/* Unity includes here. */
+using UnityEngine;
 using System.Collections;
 
-public class SCR_DetailedWindow : MonoBehaviour 
+/* Detailed window IS A UI element, therefore inherits from it. */
+public class SCR_DetailedWindow : SCR_BaseUIElement
 {
 
 	/* Attributes. */
 
 
 	/* Methods. */
-	// Use this for initialization
-	void Start () 
-	{
 
-		
 
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
-	}
 }
