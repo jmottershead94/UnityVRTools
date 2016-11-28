92803f133a95f92c1d1472065a7d74a189a518b1
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-11-28 21:56:20 +0000

    Update - Added comments to the panel class.

--- UnityVR/Assets/Scripts/UI/SCR_Panel.cs
+++ UnityVR/Assets/Scripts/UI/SCR_Panel.cs
@@ -1,10 +1,31 @@
-﻿using UnityEngine;
+﻿/*
+*
+*	Panel Class
+*	===========
+*
+*	Created: 	2016/11/21
+*	Filter:		Scripts/UI
+*	Class Name: SCR_Panel
+*	Base Class: SCR_BaseUIElement
+*	Author: 	1300455 Jason Mottershead
+*
+*	Purpose:	Panel helps to identify what game objects under the 3D menu are actually 
+*				panels, and will - later in development - provide more functionality.
+*
+*/
+
+/* Unity includes here. */
+using UnityEngine;
 using System.Collections;
 
+/* Detailed window IS A UI element, therefore inherits from it. */
 public class SCR_Panel : SCR_BaseUIElement 
 {
 
 	/* Attributes. */
-	//private bool isInFocus = false;
+
+
+	/* Methods. */
+
 	
 }
