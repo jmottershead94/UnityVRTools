093f5ac1ce7a859ad16ada0ab5a7aab389c4a2d7
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-11-28 21:55:50 +0000

    Update - Added comments to the base UI element class.

--- UnityVR/Assets/Scripts/UI/SCR_BaseUIElement.cs
+++ UnityVR/Assets/Scripts/UI/SCR_BaseUIElement.cs
@@ -1,17 +1,37 @@
-﻿using UnityEngine;
+﻿/*
+*
+*	Base UI Element Class
+*	=====================
+*
+*	Created: 	2016/11/21
+*	Filter:		Scripts/UI
+*	Class Name: SCR_BaseUIElement
+*	Base Class: Monobehaviour
+*	Author: 	1300455 Jason Mottershead
+*
+*	Purpose:	This class will provide the foundations for any UI element in this application,
+*				and also provide access to the current focus status of the element.
+*
+*/
+
+/* Unity includes here. */
+using UnityEngine;
 using System.Collections;
 
+/* Base UI element IS A game object, therefore inherits from it. */
 public class SCR_BaseUIElement : MonoBehaviour 
 {
 
 	/* Attributes. */
-	protected bool isInFocus = false;
+	protected bool isInFocus = false;	/* Used to indicate if this game object is in focus or not. */
 
-	/* Virtual Methods. */
+	/* Methods. */
+	/* Virtual. */
 	/* All UI elements must check focus. */
 	protected virtual void CheckFocus(){}
 
-	/* Methods. */
+	/* Getters/Setters. */
+	/* This will allow us to get/set the current focus status of this game object. */
 	public bool InFocus
 	{
 		get { return isInFocus; }
