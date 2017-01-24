53e5e8b8d5231ad3b883b59f7a727f16d456c6c9
jmottershead94 <jasonmottershead@hotmail.co.uk>
2017-01-24 14:22:11 +0000

    Update - Added in basic colour buttons, currently (Black, White, Red, Green and Blue). Need to properly find a way to implement a colour wheel without using the standard Unity UI.

---
+++ UnityVR/Assets/Scripts/UI/Buttons/SCR_ColourButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCR_ColourButton : SCR_3DButton 
+{
+
+	/* Attributes. */
+	[SerializeField]	private Color colour = Color.black;
+
+	/* Methods. */
+	/*
+	*
+	*	Overview
+	*	--------
+	*	This will allow us to define a specific button response.
+	*
+	*/
+	override public void ButtonPressResponse()
+	{
+
+		for(int i = 0; i < sceneEditor.Objects.Count; i++)
+		{
+
+			if(sceneEditor.Objects[i].InFocus)
+			{
+				
+				sceneEditor.Objects[i].gameObject.GetComponent<Renderer>().material.color = colour;
+				sceneEditor.Objects[i].DefaultMaterial.color = colour;
+
+			}
+
+		}
+
+	}
+}
--- UnityVR/Assets/Scripts/UI/Buttons/SCR_ColourWheel.cs
+++ UnityVR/Assets/Scripts/UI/Buttons/SCR_ColourWheel.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Microsoft.Win32;
 
 public class SCR_ColourWheel : SCR_3DButton 
 {
 
 	/* Attributes. */
 	private Color colourSelected = Color.white;
-	private SpriteRenderer spriteRenderer = null;
-	private Texture2D texture = null;
+	//private SpriteRenderer spriteRenderer = null;
+	//private Texture2D texture = null;private Texture2D texture = null;
+	private MeshRenderer meshRenderer = null;
+	private Texture texture = null;
+	private Texture2D tex = null;
 	private Color[] colours = null;
 
 	/* Methods. */
@@ -23,9 +27,27 @@ public class SCR_ColourWheel : SCR_3DButton
 
 		base.Awake();
 
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		texture = spriteRenderer.sprite.texture;
-		colours = texture.GetPixels();
+		//spriteRenderer = GetComponent<SpriteRenderer>();
+		meshRenderer = GetComponent<MeshRenderer>();
+		texture = meshRenderer.sharedMaterial.mainTexture;
+		tex = (Texture2D)texture;
+		colours = tex.GetPixels();
+
+		Debug.Log("Texture Size = " + texture.width + " " + texture.height);
+
+	}
+
+	private Color SampleTexture(float xPos, float yPos)
+	{
+		Color color = tex.GetPixel((int)(xPos / texture.width), (int)(yPos / texture.height));
+		Debug.Log("Click Pos on Wheel = " + (int)(xPos / texture.width) + " " + (int)(yPos / texture.height));
+		return color;
+	}
+
+	void OnMouseDown()
+	{
+
+		ButtonPressResponse();
 
 	}
 
@@ -41,13 +63,15 @@ public class SCR_ColourWheel : SCR_3DButton
 
 		// This would need to be either the mouse position.
 		// OR the end of the ray from the right hand controller.
-        float x = Input.mousePosition.x - transform.position.x;
-        float y = Input.mousePosition.y - transform.position.y;
+		Vector2 clickPosition = new Vector2(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y);
 
-		int x2 = (int)(x * (spriteRenderer.bounds.size.x / (spriteRenderer.bounds.size.x + 0.0f)));
-		int y2 = (int)((spriteRenderer.bounds.size.y - y) * (spriteRenderer.bounds.size.y / (spriteRenderer.bounds.size.y + 0.0f)));
+        Debug.Log("Click Pos = " + clickPosition.x + " " + clickPosition.y);
 
-        Color col = texture.GetPixel(x2, y2);
+		//int x2 = (int)(x * (spriteRenderer.bounds.size.x / (spriteRenderer.bounds.size.x + 0.0f)));
+		//int y2 = (int)((spriteRenderer.bounds.size.y - y) * (spriteRenderer.bounds.size.y / (spriteRenderer.bounds.size.y + 0.0f)));
+		Vector2 samplePosition = new Vector2(clickPosition.x / 4.0f, clickPosition.y / 4.0f);
+
+		Color col = SampleTexture((clickPosition.x / 4.0f), (clickPosition.y / 4.0f));
         colourSelected = col;
 
 		Debug.Log("RGB: " + colourSelected.r.ToString("F2") + " " + colourSelected.g.ToString("F2") + " " + colourSelected.b.ToString("F2") + " ");
