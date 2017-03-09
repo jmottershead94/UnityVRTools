88a67b66db9afa6a7c04ce622c9086a39fab59c7
jmottershead94 <jasonmottershead@hotmail.co.uk>
2017-03-09 19:12:23 +0000

    Update - Enemies now have a line of sight, and will only shoot if the player is in their range and within sight.

--- UnityVR/Assets/Examples/FPS/Scripts/Characters/SCR_Enemy.cs
+++ UnityVR/Assets/Examples/FPS/Scripts/Characters/SCR_Enemy.cs
@@ -13,9 +13,12 @@ namespace IndieJayVR
 			/// </summary>
 			public class SCR_Enemy : SCR_Character 
 			{
+				[SerializeField]	private float rayDistance = 100.0f;
 				private SCR_Player player = null;
 				private bool setupFireRate = false;
 				private bool resetRepeating = false;
+				private RaycastHit raycastTarget;
+				private Ray ray;
 
 				/// <summary>
 				/// Start this instance.
@@ -25,7 +28,7 @@ namespace IndieJayVR
 					gun = transform.FindChild ("PRE_Gun").GetComponent<SCR_Gun> ();
 					gun.InfiniteBullets = true;
 					gun.FireRate = Random.Range(1.0f, 6.0f);
-					InvokeRepeating("FireAtPlayer", SCR_GameControl.StartDelayTime, gun.FireRate * (1.0f / SCR_GameControl.SpeedFactor));
+					//InvokeRepeating("FireAtPlayer", SCR_GameControl.StartDelayTime, gun.FireRate * (1.0f / SCR_GameControl.SpeedFactor));
 				}
 
 				/// <summary>
@@ -46,6 +49,53 @@ namespace IndieJayVR
 				}
 
 				/// <summary>
+				/// Provides the enemy with the ability to have line of sight.
+				/// </summary>
+				void LineOfSight()
+				{
+					gun.transform.LookAt(player.transform.position);
+
+					// Make the enemy look at the player.
+					Vector3 tempDirection = player.transform.position - transform.position;
+					tempDirection.Normalize();
+
+					// Cast a ray from the enemy to the player.
+					ray = Camera.main.ScreenPointToRay(transform.position);
+					ray.origin = transform.position;
+					ray.direction = tempDirection;
+					Vector3 point = ray.origin + (tempDirection * rayDistance);
+
+					Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+					// If the ray has hit something.
+					if (Physics.Raycast (ray, out raycastTarget, rayDistance)) 
+					{
+						// If the ray has hit the player.
+						if (raycastTarget.collider.tag == "MainCamera") 
+						{
+							// Set the enemy to fire at the player.
+							if(!player.HasStoppedTime)
+							{
+								if(resetRepeating)
+								{
+									InvokeRepeating("FireAtPlayer", 1.0f, gun.FireRate * (1.0f / SCR_GameControl.SpeedFactor));
+									resetRepeating = false;
+								}
+							}
+						}
+						// Otherwise, the ray has hit something else.
+						else
+						{
+							if(!resetRepeating)
+							{
+								resetRepeating = true;
+								CancelInvoke();
+							}
+						}
+					}
+				}
+
+				/// <summary>
 				/// Update this instance.
 				/// </summary>
 				new protected void Update()
@@ -57,25 +107,11 @@ namespace IndieJayVR
 					else if (SCR_Player.IsReady) 
 						player = playerObject.GetComponent<SCR_Player> ();
 
-					base.Update();
-					gun.transform.LookAt(player.transform.position);
-
 					if(SCR_GameControl.IsGameOver || SCR_GameControl.IsPaused || player.HasStoppedTime)
-					{
-						resetRepeating = true;
-						CancelInvoke();
 						return;
-					}
-					else if(!player.HasStoppedTime)
-					{
-						if(resetRepeating)
-						{
-							InvokeRepeating("FireAtPlayer", 1.0f * Time.timeScale, gun.FireRate * (1.0f / SCR_GameControl.SpeedFactor));
-							resetRepeating = false;
-						}
-					}
 
-
+					base.Update();
+					LineOfSight();
 				}
 			}
 		}
