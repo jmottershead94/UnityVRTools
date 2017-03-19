61ff86c9ba166f40b30b6886650533652135c33f
jmottershead94 <jasonmottershead@hotmail.co.uk>
2017-03-19 17:51:01 +0000

    Update - Each level added to the build. Added in a victory condition, this needs testing tomorrow (when all enemies have been defeated, go to the next level).

---
+++ UnityVR/Assets/Examples/FPS/Scripts/SCR_GameEvents.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace IndieJayVR
+{
+	namespace Examples
+	{
+		namespace FPS
+		{
+			/// <summary>
+			/// Handles big game events. I.e Victory conditions.
+			/// </summary>
+			public class SCR_GameEvents : MonoBehaviour 
+			{
+				/// <summary>
+				/// Update this instance.
+				/// </summary>
+				void Update () 
+				{
+					// Check how many enemies there are in the scene.
+					GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+					// If all of the enemies have been defeated, go to the next scene.
+					if(enemies.Length <= 0)
+					{
+						int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+						SceneManager.LoadScene(nextLevel);
+					}
+				}
+			}
+		}
+	}
+}
